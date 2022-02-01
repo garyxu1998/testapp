dae67746f9d0700bd236ada0d48655f461690c5f
Gary Xu <garyxu@outlook.com>
2022-01-31 23:05:20 -0500

    Add server= app.server in app.py; finish changes in nginx.conf; add second testpic

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from testapp import *
 app = Dash(__name__)
 app.layout = html.Div([
     github_info_header(),
-    html.Img(src="assets/tvol_banner.png")
+    html.Img(src="assets/testpic.png")
 ])
-
+server = app.server
 if __name__ == '__main__':
     app.run_server(debug=True)
 
