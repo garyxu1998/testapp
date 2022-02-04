77f00b0299ca6c92778044b3942656337057c223
Gary Xu <garyxu@outlook.com>
2022-02-04 16:22:58 -0500

    correct serve_testapp.bat username.

--- serve_testapp.bat
+++ serve_testapp.bat
@@ -1,4 +1,4 @@
 cd C:\Users\vcm\Desktop\testapp
-git pull https://%TESTAPP_GIT_PAT%@github.com/JakeVestal/testapp.git
-venv\Scripts\python.exe -m pip install -r requirements.txt
+git pull https://%GIT_PAT%@github.com/garyxu1998/testapp.git
+venv\Scripts\python.exe -m pip install -r requirements.txt -q
 venv\Scripts\python.exe server.py
